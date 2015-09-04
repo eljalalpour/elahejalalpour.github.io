548b7feedb40b66f0af00b1a2983157769dab388
Elahe Jalalpour <elahejalalpoor@gmail.com>
2015-09-04 14:55:16 +0430

    Navigation menu was improved.

--- homepage/assets/js/nav.js
+++ homepage/assets/js/nav.js
@@ -1,12 +1,5 @@
 $(document).ready(function () {
-    //change the integers below to match the height of your upper dive, which I called
-    //banner.  Just add a 1 to the last number.  console.log($(window).scrollTop())
-    //to figure out what the scroll position is when exactly you want to fix the nav
-    //bar or div or whatever.  I stuck in the console.log for you.  Just remove when
-    //you know the position.
     $(window).scroll(function () {
-
-        console.log($(window).scrollTop());
 
         if ($(window).scrollTop() > 350) {
             $('#top-nav').addClass('nav-fixed-top');
